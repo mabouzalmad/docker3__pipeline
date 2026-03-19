68d9e0afedbe76ad51b3e4124969de4f2f114e37
manal <manal123@gmail.com>
2026-03-19 14:24:13 +0000

    exercice2 tp3docker

---
+++ tests/test_tasks.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../app")))
+def test_add_task():
+    add_task("learn docker")
+    assert "learn docker" in get_tasks()
+def test_multiple_tasks():
+    add_task("learn ci")
+    add_task("learn devops")
+    tasks=get_tasks()
+    assert "learn ci" in tasks
+    assert "learn devops" in tasks
